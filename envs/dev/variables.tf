80a2d86f9c353163cf41b0e84413712254ac5049
Pelumi Owolabi <54349181+PelumiOwolabi@users.noreply.github.com>
2025-10-16 13:15:44 +0100

    Update variables.tf

--- envs/dev/variables.tf
+++ envs/dev/variables.tf
@@ -1,3 +1,7 @@
-variable "bastion_ami" { type = string }
 variable "ec2_ami" { type = string }
 variable "key_name" { type = string }
+variable "bastion_ami" {
+  description = "AMI ID for the bastion host"
+  type        = string
+  default     = "ami-08c40ec9ead489470" # Example Amazon Linux 2 AMI for us-east-1
+}
